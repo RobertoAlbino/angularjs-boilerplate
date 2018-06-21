f8949a34e02424efbd5cc474187cb85854276734
Roberto Albino <junior_wash@hotmail.com>
2018-06-20 23:46:21 -0300

    Criando a estrutura inicial do frontend

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cotaEasyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cotaEasyApp
+ */
+angular.module('cotaEasyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
