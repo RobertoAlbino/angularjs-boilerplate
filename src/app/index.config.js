ea5ed75dfb6e0eafb493d682a80a0d1a23af9349
Roberto Albino <junior_wash@hotmail.com>
2018-06-25 23:26:41 -0300

    Auto Commit

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -14,7 +14,8 @@
     // Configurações toastr 
     toastrConfig.positionClass = 'toast-top-right';
     // Configurações Restangular
-    RestangularProvider.setBaseUrl('http://localhost:8080/api/');
+    //RestangularProvider.setBaseUrl('http://localhost:8080/api/'); // Desenvolvimento
+    RestangularProvider.setBaseUrl('https://cota-easy-api.herokuapp.com/api/'); // Produção
     RestangularProvider.setDefaultHeaders({ "Content-Type": 'application/json' });
   }
 })();
